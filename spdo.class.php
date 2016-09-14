9f985c14570b28b272dfaa61825f2ad21b851f36
Jalube Miguel <miguel.jalube@lausanne.ch>
2016-09-14 16:03:40 +0200

    fetch_Style + bind method

--- spdo.class.php
+++ spdo.class.php
@@ -21,22 +21,30 @@ class SPDO{
     return self::$instance;
   }
   
-  public function query($query,$bind = null){
+  public function query($query, $bind = null, $fetch = null, $fetchStyle = null, $fetchParam = null){
       try {
           $sth = $this->PDOInstance->prepare($query);
-          if($bind != null && is_array($bind)){
-              foreach($bind as $key => $value){
-                  $sth->bindValue(':'.$key, $value);
-              }
-          }
+          $this->bind($bind);
           $result = $sth->execute();
           if(preg_match('/^SELECT/',$query)){
-              return $sth->fetchAll();
+              if($fetch == 1){
+                return $sth->fetchAll($fetchStyle, $fetchParam);
+              }else{
+                return $sth->fetch($fetchStyle, $fetchParam); 
+              }
           }else{
               return $result;
           }
       } catch (PDOException $ex) {
           echo $ex->getMessage;
+      }
+  }
+  
+  private function bind($bind){
+      if($bind != null && is_array($bind)){
+          foreach($bind as $key => $value){
+              $sth->bindValue(':'.$key, $value);
+          }
       }
   }
 }
