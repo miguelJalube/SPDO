d9c6b41e9f1c5b17b92a9b429193deb3fbc76c43
Jalube Miguel <miguel.jalube@lausanne.ch>
2016-09-14 15:08:57 +0200

    adfadf

---
+++ spdo.class.php
@@ -0,0 +1,43 @@
+<?php
+namespace model;
+use PDO;
+
+class SPDO{
+  private $PDOInstance = null;  
+  private static $instance = null;
+  const DEFAULT_SQL_USER = '';
+  const DEFAULT_SQL_HOST = '';
+  const DEFAULT_SQL_PASS = '';
+  const DEFAULT_SQL_DTB = '';
+
+  public function __construct(){
+          $this->PDOInstance = new PDO('mysql:dbname='.self::DEFAULT_SQL_DTB.';host='.self::DEFAULT_SQL_HOST, self::DEFAULT_SQL_USER , self::DEFAULT_SQL_PASS);
+  }
+  
+  public static function getInstance(){  
+    if(is_null(self::$instance)){
+        self::$instance = new SPDO();
+    }
+    return self::$instance;
+  }
+  
+  public function query($query,$bind = null){
+      try {
+          $sth = $this->PDOInstance->prepare($query);
+          if($bind != null && is_array($bind)){
+              foreach($bind as $key => $value){
+                  $sth->bindValue(':'.$key, $value);
+              }
+          }
+          $result = $sth->execute();
+          if(preg_match('/^SELECT/',$query)){
+              return $sth->fetchAll();
+          }else{
+              return $result;
+          }
+      } catch (PDOException $ex) {
+          echo $ex->getMessage;
+      }
+  }
+}
+
